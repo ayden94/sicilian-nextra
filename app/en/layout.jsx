74572a83c1e9ca91b4668d227e912e7ab81ba751
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-05-07 22:35:26 +0900

    메타데이터 생성 함수에서 언어 매개변수를 제거하고, 각 언어에 맞는 메타데이터를 직접 반환하도록 수정

--- app/en/layout.jsx
+++ app/en/layout.jsx
@@ -1,7 +1,7 @@
 import metadata from "../../metadata";
 
-export async function generateMetadata({ params }) {
-  return {...metadata(await params.lang)}
+export async function generateMetadata() {
+  return {...metadata("en")}
 }
 
 export default function Layout({ children }) {
